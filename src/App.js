6cc915ad30cd4bb42f8529f748703c5a4e1e3afa
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-01 11:56:46 +0300

    add TextContainer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./Components/Header"
 import Input from "./Components/Input"
+import TextContainer from "./Components/TextContainer"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Input />
+      <TextContainer />
     </div>
   );
 }
---
+++ src/Components/TextContainer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./style.css"
+
+function Textcontainer() {
+  return (
+    <div className='textContainerDiv'>Textcontainer</div>
+  )
+}
+
+export default Textcontainer;
