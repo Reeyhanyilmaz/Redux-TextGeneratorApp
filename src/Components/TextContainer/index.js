11c55ba3506b9bde1377728a95f2808fe800640f
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-11 18:37:47 +0300

    add features

--- src/Components/TextContainer/index.js
+++ src/Components/TextContainer/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import {useEffect} from 'react';
 import "./style.css"
+import {fetchText,} from "../../Redux/Text/textSlice";
+import {useSelector, useDispatch} from "react-redux";
 
 function Textcontainer() {
+  const dispatch = useDispatch();
+
+  //parametrelerimiz
+  const paras = useSelector((state) => state.text.paras);
+  const format = useSelector((state) => state.text.format);
+
+  //container icinde yazacak olan.
+  const text = useSelector(state => state.text.items);
+
+  useEffect(() => {
+      dispatch(fetchText({paras: paras, format: format}));
+  },[dispatch, paras, format]);
+
+
   return (
-    <div className='textContainerDiv'>Textcontainer</div>
+    <div className='textContainerDiv'> {text} </div>
   )
 }
 
